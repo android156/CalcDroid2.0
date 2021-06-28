ff0d07050bbd28c02753e0cc4478ccbd69115f4c
android156 <n_degtyarev@mail.ru>
2021-06-28 16:06:22 +0300

    Добавлена обработка нажатия кнопки 2 через лиссенер. Все понятно, непонятно, как передать значение кнопки этой. Опять руками вписал Едем дальше.

--- app/src/main/java/reminder2020/remind/calcdroid20/MainActivity.java
+++ app/src/main/java/reminder2020/remind/calcdroid20/MainActivity.java
@@ -2,13 +2,15 @@ package reminder2020.remind.calcdroid20;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.view.View; //для онклик
+import android.widget.TextView; //для онклик
 import java.util.Locale;
+import android.widget.Button; // для лиссенера - способ 2
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView CalcOutput;
+//    private int counter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Получить пользовательский элемент по идентификатору
         CalcOutput = findViewById(R.id.textView);
+        Button button2 = findViewById(R.id.button_2); //способ 2
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                counter2++;
+                CalcOutput.setText(String.format(Locale.getDefault(), "Нажата кнопка %s", 2)); //counter2 убрал, руками вписал 2
+            }
+        });
     }
 
     // Обработка кнопки через атрибут onClick в макете
     public void button_1_onClick(View view) {
         CalcOutput.setText(String.format(Locale.getDefault(), "Нажата кнопка %s", 1));
     }
+
 }
