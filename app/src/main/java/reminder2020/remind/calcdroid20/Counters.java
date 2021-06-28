6d09c770170bbb1b8bec57bc5c5d13273fe79f1a
android156 <n_degtyarev@mail.ru>
2021-06-28 20:46:01 +0300

    Доработка через добавления метода View.OnClickListener. Очень много дублирующегося кода. Не верю что все нельзя закатать в функцию, которая для каждой кнопки ббудет делать аналогичные вещи. Непонятно, как передать значение кнопки этой. Опять руками вписал Едем дальше.

---
+++ app/src/main/java/reminder2020/remind/calcdroid20/Counters.java
@@ -0,0 +1,99 @@
+package reminder2020.remind.calcdroid20;
+
+public class Counters {
+    private  int counter;
+    private int counter1;
+    private int counter3;
+    private int counter2;
+    private int counter4;
+    private int counter5;
+    private int counter6;
+    private int counter7;
+    private int counter8;
+    private int counter9;
+    private int counter0;
+    private int counter_add;
+    private int counter_mlt;
+    private int counter_sub;
+    private int counter_div;
+    private int counter_point;
+
+    public Counters(){
+        counter3 = 3;
+        counter1 = 1;
+        counter2 = 2;
+        counter4 = 4;
+        counter5 = 5;
+        counter6 = 6;
+        counter7 = 7;
+        counter8 = 8;
+        counter9 = 9;
+        counter0 = 0;
+        counter_add = '+';
+        counter_mlt = '*';
+        counter_div = '/';
+        counter_sub = '-';
+        counter_point = '.';
+
+    }
+    public int getCounter() {
+        return counter;
+    }
+    public int getCounter1() {
+        return counter1;
+    }
+    public int getCounter2() {
+        return counter2;
+    }
+    public int getCounter3() {
+        return counter3;
+    }
+    public int getCounter4() {
+        return counter4;
+    }
+    public int getCounter5() {
+        return counter5;
+    }
+    public int getCounter6() {
+        return counter6;
+    }
+    public int getCounter7() {
+        return counter7;
+    }
+    public int getCounter8() {
+        return counter8;
+    }
+    public int getCounter9() {
+        return counter9;
+    }
+    public int getCounter0() {
+        return counter0;
+    }
+    public int getCounter_point() {
+        return counter_point;
+    }
+    public int getCounter_add() {
+        return counter_add;
+    }
+    public int getCounter_mlt() {
+        return counter_mlt;
+    }
+    public int getCounter_sub() {
+        return counter_sub;
+    }
+    public int getCounter_div() {
+        return counter_div;
+    }
+    public void incrementCounter1(){
+        counter1++;
+    }
+    public void incrementCounter2(){
+        counter2++;
+    }
+    public void incrementCounter3() {
+        counter3++;
+    }
+    public void incrementCounter4() {
+        counter4++;
+    }
+}
--- app/src/main/java/reminder2020/remind/calcdroid20/MainActivity.java
+++ app/src/main/java/reminder2020/remind/calcdroid20/MainActivity.java
@@ -7,30 +7,145 @@ import android.widget.TextView; //для онклик
 import java.util.Locale;
 import android.widget.Button; // для лиссенера - способ 2
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
     private TextView CalcOutput;
+    private Counters counters;
+//    private int counter1;
 //    private int counter2;
+//    private int counter3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Получить пользовательский элемент по идентификатору
+//        // Получить пользовательский элемент по идентификатору
+//        CalcOutput = findViewById(R.id.textView);
+//        Button button2 = findViewById(R.id.button_2); //способ 2
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                counter2++;
+//                CalcOutput.setText(String.format(Locale.getDefault(), "Нажата кнопка %s", 2)); //counter2 убрал, руками вписал 2
+//            }
+//        });
+        counters = new Counters();
+        initView();
+    }
+
+    private void initView() {
+// Получить пользовательские элементы по идентификатору
         CalcOutput = findViewById(R.id.textView);
-        Button button2 = findViewById(R.id.button_2); //способ 2
+
+        initButton3ClickListener();
+        initButton2ClickListener();
+        initButton4ClickListener();
+
+    }
+
+    private void initButton3ClickListener() {
+        Button button3 = findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //counters.incrementCounter3();
+                setTextCounter(CalcOutput, counters.getCounter3());
+            }
+        });
+
+    }
+    private void initButton2ClickListener() {
+        Button button2 = findViewById(R.id.button_2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                counter2++;
-                CalcOutput.setText(String.format(Locale.getDefault(), "Нажата кнопка %s", 2)); //counter2 убрал, руками вписал 2
+                //counters.incrementCounter2();
+                setTextCounter(CalcOutput, counters.getCounter2());
             }
         });
+
+    }
+
+    private void initButton4ClickListener(){
+        Button button4 = findViewById(R.id.button_4);
+        Button button5 = findViewById(R.id.button_5);
+        Button button6 = findViewById(R.id.button_6);
+        Button button7 = findViewById(R.id.button_7);
+        Button button8 = findViewById(R.id.button_8);
+        Button button9 = findViewById(R.id.button_9);
+        Button button0 = findViewById(R.id.button_0);
+        Button button_point = findViewById(R.id.button_point);
+        Button button_add = findViewById(R.id.button_add);
+        Button button_mult = findViewById(R.id.button_mult);
+        Button button_div = findViewById(R.id.button_div);
+        Button button_sub = findViewById(R.id.button_sub);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button0.setOnClickListener(this);
+        button_point.setOnClickListener(this);
+        button_add.setOnClickListener(this);
+        button_sub.setOnClickListener(this);
+        button_mult.setOnClickListener(this);
+        button_div.setOnClickListener(this);
+    }
+    // Установить текст на TextView
+    private void setTextCounter(TextView textCounter, int counter){
+        textCounter.setText(String.format(Locale.getDefault(), "%d", counter));
     }
 
     // Обработка кнопки через атрибут onClick в макете
     public void button_1_onClick(View view) {
-        CalcOutput.setText(String.format(Locale.getDefault(), "Нажата кнопка %s", 1));
+        //counters.incrementCounter1();
+        setTextCounter(CalcOutput, counters.getCounter1());
+    }
+    @Override
+    public void onClick(View v) {
+// Если на экране один пользовательский элемент, то такая обработка имеет смысл,
+// но если на экране несколько элементов, здесь придется создавать "лишние" условия.
+        switch (v.getId()) {
+            case R.id.button_4:
+                setTextCounter(CalcOutput, counters.getCounter4());
+                break;
+            case R.id.button_5:
+                setTextCounter(CalcOutput, counters.getCounter5());
+                break;
+            case R.id.button_6:
+                setTextCounter(CalcOutput, counters.getCounter6());
+                break;
+            case R.id.button_7:
+                setTextCounter(CalcOutput, counters.getCounter7());
+                break;
+            case R.id.button_8:
+                setTextCounter(CalcOutput, counters.getCounter8());
+                break;
+            case R.id.button_9:
+                setTextCounter(CalcOutput, counters.getCounter9());
+                break;
+            case R.id.button_0:
+                setTextCounter(CalcOutput, counters.getCounter0());
+                break;
+            case R.id.button_point:
+                setTextCounter(CalcOutput, counters.getCounter_point());
+                break;
+            case R.id.button_mult:
+                setTextCounter(CalcOutput, counters.getCounter_mlt());
+                break;
+            case R.id.button_add:
+                setTextCounter(CalcOutput, counters.getCounter_add());
+                break;
+            case R.id.button_sub:
+                setTextCounter(CalcOutput, counters.getCounter_sub());
+                break;
+            case R.id.button_div:
+                setTextCounter(CalcOutput, counters.getCounter_div());
+                break;
+        }
+
     }
 
 }
