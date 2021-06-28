86f603882075e2894a19f1bff18e7d3bd0b33553
android156 <n_degtyarev@mail.ru>
2021-06-28 15:29:48 +0300

    Добавлена обработка нажатия кнопки 1 через онклик. Все понятно, непонятно, как передать значение кнопки этой. Едем дальше.

--- app/src/main/java/reminder2020/remind/calcdroid20/MainActivity.java
+++ app/src/main/java/reminder2020/remind/calcdroid20/MainActivity.java
@@ -1,14 +1,25 @@
 package reminder2020.remind.calcdroid20;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView CalcOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Получить пользовательский элемент по идентификатору
+        CalcOutput = findViewById(R.id.textView);
+    }
+
+    // Обработка кнопки через атрибут onClick в макете
+    public void button_1_onClick(View view) {
+        CalcOutput.setText(String.format(Locale.getDefault(), "Нажата кнопка %s", 1));
     }
 }
